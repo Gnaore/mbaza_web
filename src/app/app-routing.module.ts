0ed791f080403bb9d769e00d07a9dd09405c370b
hgnaore <hervegnaore@gmail.com>
2024-05-03 13:11:19 +0000

    effet sur les cards Ange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
